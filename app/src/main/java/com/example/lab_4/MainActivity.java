3fecaf151f557fd276ef3d0dd78e29ef5ed62987
Rafael <a20180217@pucp.edu.pe>
2023-05-28 18:42:48 -0500

    botones

--- app/src/main/java/com/example/lab_4/MainActivity.java
+++ app/src/main/java/com/example/lab_4/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.lab_4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button ir_trabajador,ir_tutor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ir_trabajador=findViewById(R.id.ir_trabajador);
+        ir_tutor=findViewById(R.id.ir_Tutor);
+
+        ir_trabajador.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,Trabajador.class));
+            }
+        });
+
+        ir_tutor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,Tutor.class));
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/lab_4/Trabajador.java
+++ app/src/main/java/com/example/lab_4/Trabajador.java
@@ -2,13 +2,46 @@ package com.example.lab_4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Trabajador extends AppCompatActivity {
+
+    Button descargar,buscar,asignar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trabajador);
+
+        descargar=findViewById(R.id.descargar_lista);
+        buscar=findViewById(R.id.ir_buscar);
+        asignar=findViewById(R.id.ir_asignar);
+
+        descargar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        buscar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Trabajador.this,Buscar.class));
+
+            }
+        });
+
+        asignar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Trabajador.this,Asignar.class));
+
+            }
+        });
+
     }
 }
